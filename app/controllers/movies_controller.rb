b14daa038b2995dca39b5289b4f479a35ec534a0
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-31 16:44:10 +0000

    movies index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,9 +6,9 @@ class MoviesController < ApplicationController
   def show
     the_id = params.fetch("the_id")
 
-    matching_records = Director.where({ :id => the_id })
+    matching_records = Movie.where({ :id => the_id })
 
-    @the_director = matching_records.at(0)
+    @the_movie = matching_records.at(0)
 
     render({ :template => "movie_templates/details" })
   end
